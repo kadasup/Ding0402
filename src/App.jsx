ccd5cad5163a3ca8a97904943d6b607f85106e15
cyrus <cyrus.lin@gmail.com>
2026-04-03 12:36:08 +0800

    部署優化：切換至 HashRouter 以修復 GitHub Pages 顯示問題，並強化後端安全代理

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { DingProvider } from './context/DingContext';
 import Home from './pages/Home';
 import Admin from './pages/Admin';
